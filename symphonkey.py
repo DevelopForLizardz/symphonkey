685e4432f2d4fe8c833a28a8d3656b7ee226aba2
Ryan Drew <developforlizardz@gmail.com>
2017-12-02 11:00:53 -0700

    Added symphonkey script

---
+++ symphonkey.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+symphonkey.py: Turn keyboard strokes into a random symphony. Goes 
+really well with hackertyper.com
+
+When main() is run, keyboard events will be hooked to a function 
+that will pick a random Hz in the range of NOTE_RANGE and play it for 
+NOTE_LENGTH seconds. The length of time set for playing notes will 
+influence howfast one can type without experiencing notes being 
+played after one is done typing. For instance a NOTE_LENGTH of 0.5 
+second will allow for a maximum of 120 notes to be played per minute 
+((1 / 0.5) * 60), therefore supporting a max speed of 120 key strokes 
+per minute.
+
+Only dependencies are the modules pysine and keyboard, which can be 
+installed via pip (see https://github.com/libpd/libpd/issues/106 if 
+installing on OSX). Future goal would be to make this script entirely
+stand alone.
+"""
+
+import random
+import pysine
+import keyboard
+
+
+NOTE_RANGE = range(100, 7000, 100)  # hz
+NOTE_LENGTH = 0.05  # seconds
+
+
+def play_note(*args, **kwargs):
+    """
+    Selects a random frequency out of NOTE_RANGE and plays it
+    for NOTE_LENGTH seconds. Uses the module pysine.
+    """
+
+    pysine.sine(frequency=random.choice(NOTE_RANGE), duration=NOTE_LENGTH)
+
+
+def main():
+    # add the function bind, to execute play_note whenver a key is 
+    # pressed
+    keyboard.on_press(play_note)
+
+    # block until exited
+    keyboard.wait()
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        pass
